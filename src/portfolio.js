76aa13dd04196c81964e4be59e256f52ee1383e8
Justin Johnson <itzmejustin3344@gmail.com>
2021-07-05 21:29:33 -0700

    first commit

---
+++ src/portfolio.js
@@ -0,0 +1,512 @@
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello 👋.",
+  title2: "Justin",
+  logo_name: "<justin/>",
+  nickname: "jus",
+  full_name: "Justin Johnson",
+  subTitle: "Front end Developer, Open Source Enthusiast 🔥. Always learning.",
+  resumeLink:
+    "https://drive.google.com/file/d/18s1vH_-9wXQt3cTndghlcN_3rivr3Hvl/view",
+  mail: "mailto:itzjustin3344@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/itzmejus",
+  linkedin: "https://www.linkedin.com/in/justin-johnson-2179b91a9/",
+  gmail: "itzjustin3344@gmail.com",
+  instagram: "https://instagram.com/juz__98?utm_medium=copy_link",
+};
+
+const skills = {
+  data: [
+    {
+      title: "Full Stack Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly interactive Front end / User Interfaces for your web  applications",
+        "⚡ Building responsive website front end using ReactJS",
+
+        "⚡ Creating application backend in Node, Express & Flask",
+        "⚡ Integration of third party services such as Firebase/ AWS ",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        {
+          skillName: "PHP",
+          fontAwesomeClassname: "simple-icons:php",
+          style: {
+            color: "#7377AD",
+          },
+        },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+
+        {
+          skillName: "Visual Basic",
+          fontAwesomeClassname: "simple-icons:dot-net",
+          style: {
+            color: "#029FCE",
+          },
+        },
+        {
+          skillName: "MySQL",
+          fontAwesomeClassname: "simple-icons:mysql",
+          style: {
+            color: "#4479A1",
+          },
+        },
+
+        {
+          skillName: "Wordpress",
+          fontAwesomeClassname: "simple-icons:wordpress",
+          style: {
+            color: "#207297",
+          },
+        },
+        {
+          skillName: "Apache",
+          fontAwesomeClassname: "simple-icons:apache",
+          style: {
+            color: "#CA1A22",
+          },
+        },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C++",
+          fontAwesomeClassname: "simple-icons:cplusplus",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience in building websites",
+        "⚡ Experience with deploying web application",
+      ],
+      softwareSkills: [
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+          },
+        },
+
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Shreedevi College of Information Science",
+      subtitle: "Bachelor in Computer Application ",
+      logo_path: "grad.png",
+      alt_name: "SETI",
+      duration: "2018 - Present",
+      descriptions: [
+        "⚡ I have studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
+        "⚡ I have also completed various online courses for Backend , Web App Development, etc.",
+        "⚡ I have implemented several projects based on what I've leart under my Computer application course. ",
+      ],
+      website_link: "http://college.sdc.ac.in/",
+    },
+    {
+      title: "GHSS Pakkam",
+      subtitle: "Higher Secondary education ",
+      logo_path: "grad.png",
+      alt_name: "SETI",
+      duration: "2015 - 2017",
+      descriptions: [
+        "⚡ I have completed my higher secondary education in stated board on physics,chemestry maths and bio stream.",
+        "⚡ I have scored 80% on the acadamics.",
+      ],
+      website_link: "http://ursulinepublicschoolkanhiradukkam.com/",
+    },
+    {
+      title: "Ursuline Public School",
+      subtitle: "Secondary education ",
+      logo_path: "grad.png",
+      alt_name: "SETI",
+      duration: "2014 - 2015",
+      descriptions: [
+        "⚡ I have completed my secondary education in CBSE board with CGPA of 8.0.",
+        "⚡ I have participated in many cultural and sports competions held during the time.",
+      ],
+      website_link: "http://ursulinepublicschoolkanhiradukkam.com/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Pinnacle 2020",
+      subtitle: "State Level IT fest",
+      logo_path: "certif.svg",
+      certificate_link:
+        "https://drive.google.com/file/d/19Tdz99ZBxN-Sgn3dzLIgMefRPpv7jHJ9/view?usp=drivesdk",
+      alt_name: "MongoDB University",
+      // color_code: "#2AAFED",
+      color_code: "#47A048",
+    },
+    {
+      title: "Techno Tarang 2020",
+      subtitle: "State Level Annual Intercollegiate fest",
+      logo_path: "certif.svg",
+      certificate_link:
+        "https://drive.google.com/file/d/19PyZygW_6FGKOwxaquIa2ej3PmmucHuS/view?usp=drivesdk",
+      alt_name: "MongoDB University",
+      color_code: "#2AAFED",
+    },
+    {
+      title: "Scientica 2019",
+      subtitle: "National Level Science and IT fest",
+      logo_path: "certif.svg",
+      certificate_link:
+        "https://drive.google.com/file/d/19PaIyTVTSW-9swOB0Bd5F7F1i7MJrvfM/view?usp=drivesdk",
+      alt_name: "MongoDB University",
+      color_code: "#5678",
+    },
+    {
+      title: "Exuberance 2019",
+      subtitle: "Inter-collegiate management fest",
+      logo_path: "certif.svg",
+      certificate_link:
+        "https://drive.google.com/file/d/19HTl67aMr6kF_Cr0acQH8yBUiUJ2sIXq/view?usp=drivesdk",
+      alt_name: "MongoDB University",
+      color_code: "#897FED",
+    },
+    {
+      title: "Composite 2020",
+      subtitle: "National leval IT fest",
+      logo_path: "certif.svg",
+      certificate_link:
+        "https://drive.google.com/file/d/19V6208uUIqRdzxUbZPWcDPZrNJQ1kTWR/view?usp=drivesdk",
+      alt_name: "MongoDB University",
+      // color_code: "#2AAFED",
+      color_code: "#048",
+    },
+    {
+      title: "Sygma 2020",
+      subtitle: "State level IT fest",
+      logo_path: "certif.svg",
+      certificate_link:
+        "https://drive.google.com/file/d/19ahtuk389r7qUIVebkuNwggfnoT3ms27/view?usp=drivesdk",
+
+      color_code: "#2AAFED",
+    },
+
+    // color_code: "#8C151599",
+    // color_code: "#7A7A7A",
+    // color_code: "#0C9D5899",
+    // color_code: "#C5E2EE",
+    // color_code: "#ffc475",
+    // color_code: "#g",
+    // color_code: "#ffbfae",
+    // color_code: "#fffbf3",
+    // color_code: "#b190b0",
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work, and Volunteership",
+  description:
+    " I've mostly done projects on my own and I am actively looking for internships. I love organizing workshops to share my knowledge with others.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [
+        {
+          title: "Fresher",
+          company: "",
+          company_url: "",
+          logo_path: "user.svg",
+          duration: "",
+          location: "",
+          description:
+            "Seeking a beginner role to enhance and explore my technical knowledge gained from University and from various resources in the  last four years. I hold many skills to compete in the feild . .",
+          color: "#0071C5",
+        },
+      ],
+    },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "Google Local Guide",
+          company: "Google Map",
+          company_url: "https://maps.app.goo.gl/8Dq4jBHkdUcvR6D56",
+          logo_path: "localguide.png",
+          duration: "Sep 2016 - Present",
+          location: "Work From Home",
+          description:
+            "Day-to-day responsibilities of helping local businesses to spread their business to the world. Helping users by writing reviews about different locations and spaces such as shops, malls, etc. Has a photo views above 1.5 million",
+          color: "#D83B01",
+        },
+      ],
+    },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create NodeJS Backend Projects, and React Project. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "justin.jpg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "follow",
+    subtitle:
+      "I don't post frequently but when I do something awesome, I do try to post it on instagram. ",
+    link: "https://instagram.com/juz__98?utm_medium=copy_link",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "Advice generator",
+      url: "https://github.com/itzmejus/React-advice",
+      description:
+        "This is a simple app which generates random meaningfull quotes. ",
+      languages: [
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "React-amazon-clone",
+      url: "https://github.com/itzmejus",
+      description:
+        "A React Amazon Clone UI with basic functionality such as view item add to cart and delete (basic CRUD operation).",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+      ],
+    },
+
+    {
+      id: "2",
+      name: "Personal-portfolio",
+      url: "https://github.com/itzmejus",
+      description: "Personal portfolio to know more about me :).",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+      ],
+    },
+
+    {
+      id: "3",
+      name: "Netflix clone",
+      url: "https://github.com/itzmejus",
+      description: "List of Top Netflix Series which is deployed.",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "HTML5",
+          iconifyClass: "logos-html-5",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "logos-css-3",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
